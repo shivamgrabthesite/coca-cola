a390a25a33e5013ada248ebfffffeef868484a4e
Savaliya Shivam <126068849+shivamgrabthesite@users.noreply.github.com>
2023-08-08 10:41:16 +0530

    3rd commit

---
+++ lib/apis/bonus/bonus_api.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:coca_cola/constant/api.dart';
+import 'package:http/http.dart' as http;
+import '../../model/bonus_model.dart';
+import '../../model/incidence_model.dart';
+import '../../model/price_communication_model.dart';
+import '../../model/questions_model.dart';
+
+class BonusApi {
+  static Future<BonusModel?> getData(String imgId, String step) async {
+    try {
+      final url = Uri.parse(apiPath + "market/send-questions");
+      Map<String, dynamic> requestBody = {
+        "image_id": imgId,
+        "step": step,
+      };
+      var jsonencode = jsonEncode(requestBody);
+      var response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonencode,
+      );
+      print("map-------" + jsonencode);
+      print("status code----" + response.statusCode.toString());
+      // return response.body;
+      if (response.statusCode == 200) {
+        BonusModel getdata = bonusModelFromJson(response.body);
+        return getdata;
+      } else {
+        print("-------------no data found---------");
+      }
+    } catch (e) {
+      print('Error while making API request: $e');
+    }
+  }
+}
